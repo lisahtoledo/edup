0efe34d8cc98b1f59c11b488f51d661368da8961
Andre luiz <andre.bluiz94@gmail.com>
2020-01-14 12:32:45 -0300

    add react directory

---
+++ edupreact/src/main/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Head from './Header'
+
+export default function App() {
+  return (
+  <Router>
+    <Head />
+    <Routers />
+    <Footer />
+  </Router>
+  );
+}
+
---
+++ edupreact/src/main/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+        <header className="header">
+            <div className="menu logo-search">
+                    <Link to="/">
+                        <img src="./img/logo.svg" alt="">
+                    </Link>
+                <label>
+                    <input type="search" name="" id="" placeholder="Qual curso deseja?">
+                </label>
+            </div>
+            <div className="menu cad-login">
+                <Link to="">
+                    Login
+                </Link>
+                <Link to="" className="gold">
+                    Cadastre-se
+                </Link>
+            </div>
+        </header>
+  );
+}
