1196d4b4c5a78e25382dd6e8f0913cad33ea6bd6
Andre luiz <andre.bluiz94@gmail.com>
2020-01-14 21:28:31 -0300

    add imgs and icons scaphoid

--- edupreact/src/main/Header.js
+++ edupreact/src/main/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './style/header.css'
 
 export default function Header() {
   return (
         <header className="header">
             <div className="menu logo-search">
                     <Link to="/">
-                        <img src="./img/logo.svg" alt="">
+                        <img src="./img/logo.svg" alt="" />
                     </Link>
                 <label>
-                    <input type="search" name="" id="" placeholder="Qual curso deseja?">
+                    <input type="search" name="" id="" placeholder="Qual curso deseja?" />
                 </label>
             </div>
             <div className="menu cad-login">
---
+++ edupreact/src/main/Routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+
+export default function Routes() {
+  return (
+    <Switch>
+        <Route path="/" exact component="" />
+    </Switch>
+  );
+}
