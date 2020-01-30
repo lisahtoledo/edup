633c2b4dc7f5d8a6af7cc9ba100c155baa0a1c88
Andre luiz <andre.bluiz94@gmail.com>
2020-01-30 12:10:09 -0300

    add script-inclomplet DB

---
+++ backend/database/bancoDeDados.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE edup;
+
+SHOW DATABASES;
+
+USE edup;
+
+SHOW TABLES;
+
+CREATE TABLE estudante(
+	id_estudante
+    nome_completo
+    cpf
+    rg
+    orgao_expeditor
+    etinia
+    pdc
+    data_nascimento
+    sexo
+    naturalidade
+    nascionalidade
+    lgbt
+    senha
+    disponibilidade
+    
+);
