1f1b014c29d6f0d0590ca610f3a4d49d5d74eea7
Andre luiz <andre.bluiz94@gmail.com>
2020-01-31 10:04:00 -0300

    add table responsavel and foreign key

--- backend/database/bancoDeDados.sql
+++ backend/database/bancoDeDados.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE edup;
+CREATE DATABASE edup
+  CHARACTER SET utf8
+  COLLATE utf8_general_ci;
 
 SHOW DATABASES;
 
@@ -6,20 +8,40 @@ USE edup;
 
 SHOW TABLES;
 
-CREATE TABLE estudante(
-	id_estudante
-    nome_completo
-    cpf
-    rg
-    orgao_expeditor
-    etinia
-    pdc
-    data_nascimento
-    sexo
-    naturalidade
-    nascionalidade
-    lgbt
-    senha
-    disponibilidade
-    
+DROP DATABASE edup;
+
+DROP TABLE est_cad;
+DROP TABLE resp_cad;
+
+DESC est_cad;
+
+CREATE TABLE IF NOT EXISTS est_cad(
+	est_id SERIAL PRIMARY KEY,
+    est_nome VARCHAR(100) NOT NULL,
+    est_cpf VARCHAR(13) NOT NULL UNIQUE,
+    est_email VARCHAR(50) NOT NULL UNIQUE,
+    est_eti VARCHAR(20) NOT NULL,
+    est_pcd ENUM('S', 'N') NOT NULL,
+    est_data_nascimento DATE,
+    est_sexo ENUM('F', 'M', 'O'),
+    est_naturalidade VARCHAR(25) NOT NULL,
+    est_nascionalidade VARCHAR(50) NOT NULL,
+    est_lgbt ENUM('S', 'N'),
+    est_senha VARCHAR(255),
+	resp_id INT,
+    FOREIGN KEY (resp_id)
+		REFERENCES resp_cad(resp_id)
+		ON DELETE CASCADE
+);
+
+
+DESC resp_cad;
+
+CREATE TABLE IF NOT EXISTS resp_cad(
+	resp_id INT PRIMARY KEY NOT NULL,
+    resp_nome VARCHAR(100),
+	resp_prof VARCHAR(50),
+    resp_grau_esc VARCHAR(25),
+    resp_est_civ ENUM('S', 'V', 'C'),
+    resp_cpf VARCHAR(13)
 );
