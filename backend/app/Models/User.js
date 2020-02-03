79201c65476c623caee4a185e19cd99021c80860
Andre luiz <andre.bluiz94@gmail.com>
2020-02-03 11:58:25 -0300

    user can add many addresses

--- backend/app/Models/User.js
+++ backend/app/Models/User.js
@@ -17,16 +17,10 @@ class User extends Model {
     })
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
+  addresses () {
+    return this.hasMany('App/Models/UserAddress')
+  }
+
   tokens () {
     return this.hasMany('App/Models/Token')
   }
